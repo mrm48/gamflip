c420448ee37276030dd9f372c6a2b005537c5895
Matt Miller <_tripleNull@crispyecho.com>
2020-04-29 19:00:10 -0400

    Update labels to align with other GTK apps

--- gamflip.py
+++ gamflip.py
@@ -27,7 +27,7 @@ class FlipswitchWindow(Gtk.Window):
         self.combobox.set_active(0)
 
         # Row 1 (Switch)
-        label1 = Gtk.Label(label="Webcam flip state:")
+        label1 = Gtk.Label(label="Webcam flip state")
         label1.set_margin_bottom(10)
         grid.add(label1)
         flipswitch = Gtk.Switch(name="Switch")
@@ -39,14 +39,14 @@ class FlipswitchWindow(Gtk.Window):
         grid.attach_next_to(flipswitch,label1,Gtk.PositionType.RIGHT,3,1)
         
         # Row 2 - Source (Webcam)
-        label2 = Gtk.Label(label="Webcam:")
+        label2 = Gtk.Label(label="Webcam")
         label2.set_margin_bottom(5)
         self.combobox_source.set_margin_bottom(5)
         grid.attach_next_to(label2,label1,Gtk.PositionType.BOTTOM,1,2)
         grid.attach_next_to(self.combobox_source,label2,Gtk.PositionType.RIGHT,2,1)
         
         # Row 3 - Loopback Device
-        label3 = Gtk.Label(label="Loopback device:")
+        label3 = Gtk.Label(label="Loopback device")
         grid.attach_next_to(label3,label2,Gtk.PositionType.BOTTOM,1,2)
         grid.attach_next_to(self.combobox,label3,Gtk.PositionType.RIGHT,2,1)
 
